dcd56ddf68e85d51ddcc0345311a60abf61eda6e
prxt6529 <prxt@6529.io>
2024-08-12 13:09:05 +0300

    WIP
    Signed-off-by: prxt6529 <prxt@6529.io>

--- renderer/components/pdfViewer/PdfViewer.tsx
+++ renderer/components/pdfViewer/PdfViewer.tsx
@@ -1,14 +1,19 @@
+import styles from "./PdfViewer.module.scss";
+import { Document, Page, pdfjs } from "react-pdf";
 import { useEffect, useRef, useState } from "react";
 import { Col, Container, Row } from "react-bootstrap";
+import "react-pdf/dist/esm/Page/AnnotationLayer.css";
+import "react-pdf/dist/esm/Page/TextLayer.css";
+import Pagination from "../pagination/Pagination";
 
 interface Props {
   file: string;
 }
 
 export default function PdfViewer(props: Readonly<Props>) {
-  // useEffect(() => {
-  //   pdfjs.GlobalWorkerOptions.workerSrc = "/pdf.worker.min.mjs";
-  // }, []);
+  useEffect(() => {
+    pdfjs.GlobalWorkerOptions.workerSrc = "/pdf.worker.min.mjs";
+  }, []);
 
   const [numPages, setNumPages] = useState(0);
   const [pageNumber, setPageNumber] = useState(1);
@@ -30,12 +35,39 @@ export default function PdfViewer(props: Readonly<Props>) {
     <Container className="no-padding" ref={containerRef}>
       <Row>
         <Col>
-          <iframe
-            src={props.file}
-            width="100%"
-            style={{
-              height: "90vh",
-            }}></iframe>
+          <Document
+            file={props.file}
+            onLoadSuccess={onDocumentLoadSuccess}
+            onLoadError={(e) => {
+              console.log("Error loading PDF", e);
+            }}>
+            {isLoading && renderedPageNumber ? (
+              <Page
+                key={renderedPageNumber}
+                className={styles.prevPage}
+                pageNumber={renderedPageNumber}
+                width={containerRef.current?.offsetWidth}
+              />
+            ) : null}
+            <Page
+              key={pageNumber}
+              pageNumber={pageNumber}
+              onRenderSuccess={() => setRenderedPageNumber(pageNumber)}
+              width={containerRef.current?.offsetWidth}
+            />
+          </Document>
+        </Col>
+      </Row>
+      <Row className="pt-2">
+        <Col className="d-flex justify-content-center gap-2">
+          <Pagination
+            page={pageNumber}
+            pageSize={1}
+            totalResults={numPages}
+            setPage={function (newPage: number) {
+              changePage(newPage);
+            }}
+          />
         </Col>
       </Row>
     </Container>
