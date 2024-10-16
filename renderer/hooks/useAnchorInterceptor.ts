ba6f5a8f98211ded82609f46be15d0510f203091
prxt6529 <prxt@6529.io>
2024-10-16 11:41:11 +0300

    WIP
    Signed-off-by: prxt6529 <prxt@6529.io>

--- renderer/hooks/useAnchorInterceptor.ts
+++ renderer/hooks/useAnchorInterceptor.ts
@@ -2,29 +2,29 @@ import { useEffect } from "react";
 import { openInExternalBrowser } from "../helpers";
 import { SEIZE_URL } from "../../constants";
 
+export const handleAnchorClick = (event: any) => {
+  const target = event.target.closest("a");
+
+  if (target && target.tagName === "A") {
+    const href = target.href;
+    const isExternalLink = target.target === "_blank";
+    if (isExternalLink) {
+      event.preventDefault();
+      if (
+        href.startsWith(SEIZE_URL) ||
+        href.startsWith("/") ||
+        href.includes("localhost")
+      ) {
+        window.location.href = href.replace(SEIZE_URL, "");
+      } else {
+        openInExternalBrowser(href);
+      }
+    }
+  }
+};
+
 export const useAnchorInterceptor = () => {
   useEffect(() => {
-    const handleAnchorClick = (event: any) => {
-      const target = event.target.closest("a");
-
-      if (target && target.tagName === "A") {
-        const href = target.href;
-        const isExternalLink = target.target === "_blank";
-        if (isExternalLink) {
-          event.preventDefault();
-          if (
-            href.startsWith(SEIZE_URL) ||
-            href.startsWith("/") ||
-            href.includes("localhost")
-          ) {
-            window.location.href = href.replace(SEIZE_URL, "");
-          } else {
-            openInExternalBrowser(href);
-          }
-        }
-      }
-    };
-
     document.addEventListener("click", handleAnchorClick);
 
     return () => {
