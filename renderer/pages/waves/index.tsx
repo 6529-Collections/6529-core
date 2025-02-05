8f8a17b92713a0f8510b4750a464173a1e04861e
Ragne <ragneinfo@gmail.com>
2025-02-05 13:56:05 +0200

    added missing waves index file to solve 404 issue

---
+++ renderer/pages/waves/index.tsx
@@ -0,0 +1,54 @@
+import Head from "next/head";
+import Breadcrumb, { Crumb } from "../../components/breadcrumb/Breadcrumb";
+import Waves from "../../components/waves/Waves";
+import dynamic from "next/dynamic";
+import HeaderPlaceholder from "../../components/header/HeaderPlaceholder";
+import { AuthContext } from "../../components/auth/Auth";
+import { useContext, useEffect } from "react";
+const Header = dynamic(() => import("../../components/header/Header"), {
+  ssr: false,
+  loading: () => <HeaderPlaceholder />,
+});
+
+export default function WavesPage() {
+  const { setTitle, title } = useContext(AuthContext);
+  const breadcrumbs: Crumb[] = [
+    { display: "Home", href: "/" },
+    { display: "Waves" },
+  ];
+
+  useEffect(() => {
+    setTitle({
+      title: "Waves | 6529 SEIZE",
+    });
+  }, []);
+
+  return (
+    <>
+      <Head>
+        <title>{title}</title>
+        <link rel="icon" href="/favicon.ico" />
+        <meta name="description" content="Waves | 6529 SEIZE" />
+        <meta
+          property="og:url"
+          content={`${process.env.BASE_ENDPOINT}/waves`}
+        />
+        <meta property="og:title" content="Waves" />
+        <meta
+          property="og:image"
+          content={`${process.env.BASE_ENDPOINT}/Seize_Logo_Glasses_2.png`}
+        />
+        <meta property="og:description" content="6529 SEIZE" />
+      </Head>
+      <div className="tailwind-scope lg:tw-min-h-screen tw-bg-iron-950 tw-overflow-x-hidden">
+        <div>
+          <Header />
+          <Breadcrumb breadcrumbs={breadcrumbs} />
+        </div>
+        <div className="tw-overflow-hidden tw-h-full tw-w-full">
+          <Waves />
+        </div>
+      </div>
+    </>
+  );
+}
