67e18cf0de6a9183469cbdb662a6f338f6f41144
prxt6529 <prxt@6529.io>
2025-04-08 17:15:20 +0300

    WIP
    Signed-off-by: prxt6529 <prxt@6529.io>

--- sign-win/win-update-latest.ts
+++ sign-win/win-update-latest.ts
@@ -2,6 +2,10 @@ import * as fs from "fs";
 import * as path from "path";
 import * as crypto from "crypto";
 import * as yaml from "js-yaml";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const distDir = path.join(__dirname, "../dist");
 
