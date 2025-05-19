9d47eb3c4180d468441919145290654a594840e2
prxt6529 <prxt@6529.io>
2025-05-19 16:20:00 +0300

    WIP
    Signed-off-by: prxt6529 <prxt@6529.io>

--- electron-src/utils/env.ts
+++ electron-src/utils/env.ts
@@ -3,3 +3,11 @@ import Logger from "electron-log";
 export const isDev = process.argv.some((str) => str == "--dev");
 
 Logger.info("MODE:", isDev ? "DEV" : "PROD");
+
+if (!isDev) {
+  console.log = (...args) => Logger.info(...args);
+  console.info = (...args) => Logger.info(...args);
+  console.warn = (...args) => Logger.warn(...args);
+  console.error = (...args) => Logger.error(...args);
+  console.debug = (...args) => Logger.debug(...args);
+}
