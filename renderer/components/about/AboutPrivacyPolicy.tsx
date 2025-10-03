282a0ea1cac0ac4ba9c0aab69636d94e771535a7
prxt6529 <prxt@6529.io>
2025-10-03 11:29:02 +0300

    WIP
    Signed-off-by: prxt6529 <prxt@6529.io>

--- renderer/components/about/AboutPrivacyPolicy.tsx
+++ renderer/components/about/AboutPrivacyPolicy.tsx
@@ -1,7 +1,6 @@
-import Link from "next/link";
-import styles from "./About.module.scss";
-import { Col, Container, Row } from "react-bootstrap";
 import { AboutSection } from "@/enums";
+import { Col, Container, Row } from "react-bootstrap";
+import styles from "./About.module.scss";
 
 export default function AboutPrivacyPolicy() {
   return (
@@ -138,16 +137,12 @@ export default function AboutPrivacyPolicy() {
               <b>Cookies.</b> Some of our automatic data collection is
               facilitated by cookies and similar technologies. For more
               information, see our{" "}
-<<<<<<< HEAD
-              <Link href={`/about/${AboutSection.COOKIE_POLICY}`}>
-=======
               <a
                 href={`/about/${AboutSection.COOKIE_POLICY}`}
                 target="_blank"
                 rel="noopener noreferrer">
->>>>>>> 230272bfd7d4f6b8cc6e8c57622d123389f1280e
                 Cookie Policy
-              </Link>
+              </a>
               . We may also store a record of your preferences in respect of the
               use of these technologies in connection with the Service.
             </p>
@@ -301,16 +296,12 @@ export default function AboutPrivacyPolicy() {
             <p>
               <b>Cookies.</b> For information about cookies employed by the
               Service and how to control them, see our{" "}
-<<<<<<< HEAD
-              <Link href={`/about/${AboutSection.COOKIE_POLICY}`}>
-=======
               <a
                 href={`/about/${AboutSection.COOKIE_POLICY}`}
                 target="_blank"
                 rel="noopener noreferrer">
->>>>>>> 230272bfd7d4f6b8cc6e8c57622d123389f1280e
                 Cookie Policy
-              </Link>
+              </a>
               .
             </p>
             <p>
@@ -396,16 +387,12 @@ export default function AboutPrivacyPolicy() {
               age. If you are a parent or guardian of a child from whom you
               believe we have collected personal information in a manner
               prohibited by law, please{" "}
-<<<<<<< HEAD
-              <Link href={`/about/${AboutSection.CONTACT_US}`}>contact us</Link>
-=======
               <a
                 href={`/about/${AboutSection.CONTACT_US}`}
                 target="_blank"
                 rel="noopener noreferrer">
                 contact us
               </a>
->>>>>>> 230272bfd7d4f6b8cc6e8c57622d123389f1280e
               . If we learn that we have collected personal information through
               the Service from a child without the consent of the child&apos;s
               parent or guardian as required by law, we will comply with
