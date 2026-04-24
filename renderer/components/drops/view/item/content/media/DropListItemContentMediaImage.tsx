43bab722a092725b0441ce4dada5fe2ca121f47f
prxt6529 <prxt@6529.io>
2026-04-24 15:30:03 +0300

    WIP
    Signed-off-by: prxt6529 <prxt@6529.io>

--- renderer/components/drops/view/item/content/media/DropListItemContentMediaImage.tsx
+++ renderer/components/drops/view/item/content/media/DropListItemContentMediaImage.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import { FallbackImage } from "@/components/common/FallbackImage";
+import { openInExternalBrowser } from "@/helpers";
 import { fullScreenSupported } from "@/helpers/Helpers";
 import { getScaledImageUri, ImageScale } from "@/helpers/image.helpers";
 import useCapacitor from "@/hooks/useCapacitor";
@@ -12,7 +13,6 @@ import {
   ArrowTopRightOnSquareIcon,
   InformationCircleIcon,
 } from "@heroicons/react/24/outline";
-import Link from "next/link";
 import React, { useCallback, useRef, useState } from "react";
 import { createPortal } from "react-dom";
 import { Tooltip } from "react-tooltip";
@@ -195,16 +195,17 @@ function DropListItemContentMediaImage({
                     />
                   </button>
                 )}
-                <Link href={src} target="_blank" rel="noopener noreferrer">
-                  <button
-                    onClick={(e) => e.stopPropagation()}
-                    data-tooltip-id={`open-browser-${src}`}
-                    className={modalButtonClasses}
-                    aria-label="Open image in new tab"
-                  >
-                    <ArrowTopRightOnSquareIcon className="tw-h-5 tw-w-5 tw-flex-shrink-0" />
-                  </button>
-                </Link>
+                <button
+                  onClick={(e) => {
+                    e.stopPropagation();
+                    openInExternalBrowser(src);
+                  }}
+                  data-tooltip-id={`open-browser-${src}`}
+                  className={modalButtonClasses}
+                  aria-label="Open image in new tab"
+                >
+                  <ArrowTopRightOnSquareIcon className="tw-h-5 tw-w-5 tw-flex-shrink-0" />
+                </button>
                 {fullScreenSupported() && !isCapacitor && (
                   <button
                     onClick={handleFullScreen}
